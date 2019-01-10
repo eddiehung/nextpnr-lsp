9b26bc4cc809734cb3e32f96398cf24ae48c2671
Eddie Hung <eddieh@ece.ubc.ca>
2019-01-10 13:30:33 -0800

    Fix typo

--- ice40/RUNME.sh
+++ ice40/RUNME.sh
@@ -1,5 +1,5 @@
 bash shortest.sh |& tee shortest.log
-bash longest_in_10s.sh |& tee longest_in_10s.log
-bash longest_in_30s.sh |& tee longest_in_30s.log
-bash longest_in_300s.sh |& tee longest_in_300s.log
-bash longest_in_3000s.sh |& tee longest_in_3000s.log
+bash longest_in_10.sh |& tee longest_in_10.log
+bash longest_in_30.sh |& tee longest_in_30.log
+bash longest_in_300.sh |& tee longest_in_300.log
+bash longest_in_3000.sh |& tee longest_in_3000.log
